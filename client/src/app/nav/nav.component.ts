21dbd6c289efe61a8c6672ebc82f5c3b92da3bc5
DanielODH <pollodany02@gmail.com>
2024-10-08 07:12:05 -0600

    Parcial02 - Add routing in code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { FormsModule} from '@angular/forms';
 import { inject } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'; 
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -14,13 +14,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model:any={};
 
   login(): void {
     this.accountService.login(this.model).subscribe(
       {
-        next: (response) => {
-          console.log(response);
+        next: _ => {
+          this.router.navigateByUrl("/members");
         },
         error: (error) => {
           console.log(error);
@@ -30,5 +31,6 @@ export class NavComponent {
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
