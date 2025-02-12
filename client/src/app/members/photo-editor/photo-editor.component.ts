d1a11c5d4c56c3a5af2341d6a5f2da93f7666587
DanielODH <pollodany02@gmail.com>
2025-02-11 22:34:07 -0600

    Parcial04 - Set Main Photo in Client

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -4,6 +4,8 @@ import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { FileUploader, FileUploadModule } from 'ng2-file-upload';
 import { AccountService } from '../../_services/account.service';
 import { environment } from '../../../environments/environment';
+import { MembersService } from '../../_services/members.service';
+import { Photo } from '../../_models/photo';
 
 @Component({
   selector: 'app-photo-editor',
@@ -15,17 +17,41 @@ import { environment } from '../../../environments/environment';
 
 export class PhotoEditorComponent implements OnInit {
   private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+
   member = input.required<Member>();
   uploader?: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
   memberChange = output<Member>();
+
   ngOnInit(): void {
     this.initializeUploader();
   }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo).subscribe({
+      next: _ => {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        const updatedMember = { ...this.member() };
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id == photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      }
+    });
+  }
+
   fileOverBase(e: any) {
     this.hasBaseDropZoneOver = e;
   }
+
   initializeUploader() {
     this.uploader = new FileUploader({
       url: this.baseUrl + "users/photo",
@@ -36,9 +62,11 @@ export class PhotoEditorComponent implements OnInit {
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
     });
+
     this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false
     };
+    
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       const photo = JSON.parse(response);
       const updatedMember = { ...this.member() };
