1a7051a3bdc2707bd954821ff28b131fbd098d6f
DanielODH <pollodany02@gmail.com>
2025-02-16 13:22:32 -0600

    Parcial04 - Add Custom Validator

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { JsonPipe } from '@angular/common';
@@ -11,13 +11,12 @@ import { JsonPipe } from '@angular/common';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-
 export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
-  private toastr = inject(ToastrService)
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   model: any = {};
-  registerForm: FormGroup= new FormGroup({});
+  registerForm: FormGroup = new FormGroup({});
 
   ngOnInit(): void {
     this.initializeForm();
@@ -27,8 +26,18 @@ export class RegisterComponent implements OnInit {
     this.registerForm = new FormGroup({
       username: new FormControl("Hola", Validators.required),
       password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl("", [Validators.required]),
+      confirmPassword: new FormControl("", [Validators.required, this.matchValues("password")])
     });
+
+    this.registerForm.controls["password"].valueChanges.subscribe({
+      next: () => this.registerForm.controls["confirmPassword"].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { isMatching: true }
+    };
   }
 
   register(): void {
