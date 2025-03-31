7b25b9e4a739e5bf4b86688dfed4a38e73991fa3
DanielODH <pollodany02@gmail.com>
2025-03-31 10:05:52 -0600

    Parcial05 - Add Style to Messages

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,26 +1,28 @@
 import { Component, inject, input, OnInit } from '@angular/core';
- import { MessagesService } from '../../_services/messages.service';
- import { Message } from '../../_models/message';
+import { MessagesService } from '../../_services/messages.service';
+import { Message } from '../../_models/message';
+import { TimeagoModule } from 'ngx-timeago';
+
+@Component({
+  selector: 'app-member-messages',
+  standalone: true,
+  imports: [TimeagoModule],
+  templateUrl: './member-messages.component.html',
+  styleUrl: './member-messages.component.css'
+})
+
+export class MemberMessagesComponent implements OnInit {
+  private messagesService = inject(MessagesService);
+  username = input.required<string>();
+  messages: Message[] = [];
  
- @Component({
-   selector: 'app-member-messages',
-   standalone: true,
-   imports: [],
-   templateUrl: './member-messages.component.html',
-   styleUrl: './member-messages.component.css'
- })
- export class MemberMessagesComponent implements OnInit {
-   private messagesService = inject(MessagesService);
-   username = input.required<string>();
-   messages: Message[] = [];
+  ngOnInit(): void {
+    this.loadMessages();
+  }
  
-   ngOnInit(): void {
-     this.loadMessages();
-   }
- 
-   loadMessages() {
-     this.messagesService.getMessageThread(this.username()).subscribe({
-       next: messages => this.messages = messages
-     });
-   }
+  loadMessages() {
+    this.messagesService.getMessageThread(this.username()).subscribe({
+      next: messages => this.messages = messages
+    });
+  }
  }
