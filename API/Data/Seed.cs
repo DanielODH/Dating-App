16b1bbb7f8e401bc3a69bc7eb07071eb102f5c99
DanielODH <pollodany02@gmail.com>
2025-05-19 07:37:25 -0600

    Parcial06 - Add Identity Roles

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -8,7 +8,7 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+    public static async Task SeedUsersAsync(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync())
         {
@@ -24,10 +24,35 @@ public class Seed
             return;
         }
 
+        var roles = new List<AppRole>
+        {
+            new() { Name = "Admin" },
+            new() { Name = "Member" },
+            new() { Name = "Moderator" }
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
+
+        var admin = new AppUser
+        {
+            UserName = "admin",
+            KnownAs = "Admin",
+            Gender = string.Empty,
+            City = string.Empty,
+            Country = string.Empty
+        };
+
+        await userManager.CreateAsync(admin, "Pa$$w0rd");
+        await userManager.AddToRolesAsync(admin, ["Admin", "Moderator"]);
+
         foreach (var user in users)
         {
             user.UserName = user.UserName!.ToLowerInvariant();
             await userManager.CreateAsync(user, "Pa$$w0rd");
+            await userManager.AddToRoleAsync(user, "Member");
         }
 
     }
