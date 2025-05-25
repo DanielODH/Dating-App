63766491454aaf24ae0d412f93e32966ebaf758a
DanielODH <pollodany02@gmail.com>
2025-05-24 21:01:42 -0600

    Parcial06 - Add UTC Date Formats

--- API/Helpers/AutoMapperProfile.cs
+++ API/Helpers/AutoMapperProfile.cs
@@ -9,7 +9,7 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<AppUser, MemberResponse>()
-            .ForMember(d => d.Age, 
+            .ForMember(d => d.Age,
                 o => o.MapFrom(s => s.BirthDay.CalculateAge()))
             .ForMember(
                 d => d.PhotoUrl,
@@ -25,5 +25,9 @@ public class AutoMapperProfiles : Profile
                 o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(p => p.IsMain)!.Url))
             .ForMember(d => d.RecipientPhotoUrl,
                 o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(p => p.IsMain)!.Url));
+        CreateMap<DateTime, DateTime>()
+            .ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>()
+            .ConvertUsing(d => d.HasValue ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
     }
 }
